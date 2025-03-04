0a5883ae3c93ff954886b5b45dab9cae3db0e0ef
Your Name <you@example.com>
2025-03-04 14:00:03 +0530

    added the helmt

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import userRouter from "./routes/user.route.js"
 import connectDB from "./dbconfig/db.js"
 import errorHandler from "./middlewares/errorHandler.js"
 import userMainRouter from "./routes/user.main.routes.js"
+import helmet from "helmet"
 dotenv.config()
 
 
@@ -14,6 +15,7 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(helmet());
 
 app.use("/api",userRouter);
 app.use("/user",userMainRouter)
